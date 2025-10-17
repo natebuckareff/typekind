2330447f924ea0fb02a837ba30f978e4b314bc20
Nate Buckareff <nate.buckareff@gmail.com>
2025-10-16 19:58:59 -0700

    Builder API and index for v2

---
+++ src/v2/builder.ts
@@ -0,0 +1,67 @@
+import { AnyCodec } from './codec.js';
+import { ArrayCodec } from './codecs/array.js';
+import { BigIntCodec } from './codecs/bigint.js';
+import { ChoiceCodec, ChoiceSpec } from './codecs/choice.js';
+import { DateCodec } from './codecs/date.js';
+import { I32Codec } from './codecs/i32.js';
+import { I64Codec } from './codecs/i64.js';
+import { ObjectCodec, ObjectSpec } from './codecs/object.js';
+import { OptionCodec } from './codecs/option.js';
+import { KeyCodec, RecordCodec } from './codecs/record.js';
+import { U32Codec } from './codecs/u32.js';
+import { U64Codec } from './codecs/u64.js';
+
+import {
+  BoolCodec,
+  NullCodec,
+  NumberCodec,
+  StringCodec,
+} from './codecs/simple.js';
+
+export const rec = <C extends AnyCodec>(fn: () => C) => fn();
+
+export const i32 = () => new I32Codec();
+export const i64 = () => new I64Codec();
+export const u32 = () => new U32Codec();
+export const u64 = () => new U64Codec();
+
+const _null = () => new NullCodec();
+const _bool = () => new BoolCodec();
+const _number = () => new NumberCodec();
+const _string = () => new StringCodec();
+const _bigint = () => new BigIntCodec();
+
+export {
+  _bigint as bigint,
+  _bool as bool,
+  _null as null,
+  _number as number,
+  _string as string,
+};
+
+export const date = () => new DateCodec();
+
+export const array = <S extends AnyCodec>(spec: S) => {
+  return new ArrayCodec(spec);
+};
+
+export const choice = <S extends ChoiceSpec>(spec: S) => {
+  return new ChoiceCodec(spec);
+};
+
+export const option = <S extends AnyCodec>(spec: S) => {
+  return new OptionCodec(spec);
+};
+
+export const record = <Key extends KeyCodec<any>, Value extends AnyCodec>(
+  key: Key,
+  value: Value,
+) => {
+  return new RecordCodec(key, value);
+};
+
+const _object = <S extends ObjectSpec>(spec: S) => {
+  return new ObjectCodec(spec);
+};
+
+export { _object as object };
---
+++ src/v2/index.ts
@@ -0,0 +1,21 @@
+export * as tk from './builder.js';
+
+export * from './codec-error.js';
+export * from './codec.js';
+export * from './context.js';
+export * from './json.js';
+export * from './schema.js';
+export * from './util.js';
+
+export * from './codecs/array.js';
+export * from './codecs/bigint.js';
+export * from './codecs/choice.js';
+export * from './codecs/date.js';
+export * from './codecs/i32.js';
+export * from './codecs/i64.js';
+export * from './codecs/object.js';
+export * from './codecs/option.js';
+export * from './codecs/record.js';
+export * from './codecs/simple.js';
+export * from './codecs/u32.js';
+export * from './codecs/u64.js';
