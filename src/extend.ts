80494e386bbeb0336cc86b24de63274558218092
Nate Buckareff <nate.buckareff@gmail.com>
2025-10-26 19:37:57 -0700

    Improve extendTypeKind with new builder

--- src/extend.ts
+++ src/extend.ts
@@ -1,11 +1,26 @@
+import type { Builder } from './builder.js';
 import { type AnyCodec, tk } from './index.js';
 
 export interface Extensions {
-  [key: string]: (...args: any[]) => AnyCodec;
+  [key: string]: Builder<AnyCodec, any[]>;
 }
 
-export const extendTypeKind = <Ext extends Extensions>(
-  fn: () => Ext,
-): typeof tk & Ext => {
-  return { ...tk, ...fn() };
-};
+export function extendTypeKind<Ext extends Extensions>(
+  ext: Ext,
+): typeof tk & Ext;
+
+export function extendTypeKind<Base extends Extensions, Ext extends Extensions>(
+  base: Base,
+  ext: Ext,
+): Base & Ext;
+
+export function extendTypeKind(
+  extOrBase: Extensions,
+  ext?: Extensions,
+): Extensions {
+  if (ext === undefined) {
+    return { ...tk, ...extOrBase };
+  } else {
+    return { ...extOrBase, ...ext };
+  }
+}
